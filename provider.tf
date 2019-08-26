c5e21cc25c40898929f7629a00fbb50575092c67
munireddy <munisekhar.reddy@gmail.com>
2019-08-25 23:33:34 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+  subscription_id = "6a47561d-9fe1-414e-85b7-99cd2ce0ce46"
+  tenant_id       = "59b2865a-7fb8-4ccb-ab68-72cbca88fc48"
+  client_id       = "3d587273-dcac-47f1-b714-72f244c67c52"
+  client_secret   = "8ec3a20c-e55e-4341-abec-28d57eab3be1"
+}
