c00cde94b518491a0eecbd845c330f7e91f8079f
munireddy <munisekhar.reddy@gmail.com>
2019-08-25 22:27:53 -0400

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=1.28.0"
+
+  subscription_id = "6a47561d-9fe1-414e-85b7-99cd2ce0ce46"
+  tenant_id       = "59b2865a-7fb8-4ccb-ab68-72cbca88fc48"
+}
+
+
 resource "azurerm_resource_group" "vmss" {
  name     = "${var.resource_group_name}"
  location = "${var.location}"
