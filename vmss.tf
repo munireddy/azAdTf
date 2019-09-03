7be6edba56dde3e37e15b6f41dca7bb95ab28075
munireddy <munisekhar.reddy@gmail.com>
2019-09-03 09:37:53 -0400

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -153,7 +153,7 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
     type_handler_version = "1.0"
  }
 provisioner "local-exec" {
-    command = "az login --service-principal --username \"3d587273-dcac-47f1-b714-72f244c67c52\"  --password \"8ec3a20c-e55e-4341-abec-28d57eab3be1\" --tenant \"59b2865a-7fb8-4ccb-ab68-72cbca88fc48\" && az role assignment create --role \"Virtual Machine Administrator Login\" --assignee \"nineuser@autonomicpro.com\""
+    command = "az login --service-principal --username \"3d587273-dcac-47f1-b714-72f244c67c52\"  --password \"8ec3a20c-e55e-4341-abec-28d57eab3be1\" --tenant \"59b2865a-7fb8-4ccb-ab68-72cbca88fc48\" "
 }
 
  tags = "${var.tags}"
