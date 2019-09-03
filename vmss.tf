56bfae71dac0e081430581571304f0bbed847b29
munireddy <munisekhar.reddy@gmail.com>
2019-09-03 09:51:37 -0400

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -156,6 +156,10 @@ provisioner "local-exec" {
     command = "az login --service-principal --username \"3d587273-dcac-47f1-b714-72f244c67c52\"  --password \"8ec3a20c-e55e-4341-abec-28d57eab3be1\" --tenant \"59b2865a-7fb8-4ccb-ab68-72cbca88fc48\" "
 }
 
+provisioner "local-exec" { 
+    command = "az role assignment create --role \"Virtual Machine Administrator Login\" --assignee \"nineuser@autonomicpro.com\"" 
+}
+
  tags = "${var.tags}"
 }
 
