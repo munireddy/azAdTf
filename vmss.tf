9fa5dd8b8171efd5c2c3b937e9bce6ffb85555c7
munireddy <munisekhar.reddy@gmail.com>
2019-09-02 23:48:19 -0400

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -125,7 +125,7 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
       sudo wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
       unzip terraform_0.12.7_linux_amd64.zip
       sudo mv terraform /usr/local/bin/
-      #cp provider.tf  /home/"${var.admin_user}"
+      #cp provider.tf  /home/"${var.admin_user}" #
       EOF
  }
 
