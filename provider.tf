528bb6ee2970370a396adf89bd6ea615f70e02fe
munireddy <munisekhar.reddy@gmail.com>
2019-09-07 08:30:11 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   subscription_id = "01b14a6a-401e-4a96-87d7-76265c489bd3"
   tenant_id       = "59b2865a-7fb8-4ccb-ab68-72cbca88fc48"
-  client_id       = "22d49a28-7a50-4ae0-b4b0-669f4d5157b5"
+  client_id       = "2e82de72-b22a-4145-88a5-0e49ea4d89bf"
   client_secret   = "O3REtS1b]Si2iTApJVP_6Lonzw[*y6bP"
 }
